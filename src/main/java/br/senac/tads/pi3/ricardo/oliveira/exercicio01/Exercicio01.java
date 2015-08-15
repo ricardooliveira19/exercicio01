d6a9bbef3d19ec4be2d8843a3f4bd3e90101084c
daniel.rodrigues1 <daniel.rodrigues1@CAS486190C14727.senacsp.edu.br>
2015-08-14 22:10:03 -0300

    Inclusao do metodo main

--- src/main/java/br/senac/tads/pi3/ricardo/oliveira/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/ricardo/oliveira/exercicio01/Exercicio01.java
@@ -5,6 +5,8 @@
  */
 package br.senac.tads.pi3.ricardo.oliveira.exercicio01;
 
+
+import java.util.Scanner;
 /**
  *
  * @author ricardo.oliveira5
@@ -12,6 +14,33 @@ package br.senac.tads.pi3.ricardo.oliveira.exercicio01;
 public class Exercicio01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wdddorld! ;)");
+        Scanner leitor = new Scanner(System.in);
+        int op;
+        
+        do{
+        System.out.println("1 cadastrar");
+        System.out.println("2 sair");
+        op = leitor.nextInt();
+        
+        if(op == 1){
+        System.out.println("Nome");
+        leitor.nextLine(); 
+        String nome = leitor.nextLine(); 
+        System.out.println("DataNascimento");
+        int nasc = leitor.nextInt();
+        System.out.println("E-mail");
+        leitor.nextLine(); 
+        String email = leitor.nextLine();
+        System.out.println("Telefone");
+        int tel = leitor.nextInt();
+        
+        System.out.println("\n"+nome);
+        System.out.println(nasc);
+        System.out.println(email);
+        System.out.println(tel);
+        }
+        }while(op != 2);
+            
+    
     }
 }
