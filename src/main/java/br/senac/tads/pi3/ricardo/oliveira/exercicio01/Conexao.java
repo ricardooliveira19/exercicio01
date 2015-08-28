9fe2316e3ee7c4a4eefc09e3340afd6022511d5c
ricardo.oliveira <ricardo.oliveira@SASPO-E00XPYQ-L.sa.cushwake.gbl>
2015-08-28 11:00:36 -0300

    Criado duas novas funções (conecta e removerPessoa) Conecta - otimização de códigos repetidos removerPessoa - Opão de remoção de cadastro

--- src/main/java/br/senac/tads/pi3/ricardo/oliveira/exercicio01/Conexao.java
+++ src/main/java/br/senac/tads/pi3/ricardo/oliveira/exercicio01/Conexao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.senac.tads.pi3.ricardo.oliveira.exercicio01;
 
 import java.sql.Connection;
@@ -18,174 +13,145 @@ import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author daniel.rodrigues1
  */
 public class Conexao {
-    
-  private Connection obterConexao() throws SQLException, ClassNotFoundException {
-    Connection conn = null;
-    // Passo 1: Registrar driver JDBC.
-   
-    Class.forName("org.apache.derby.jdbc.ClientDataSource");
 
-    // Passo 2: Abrir a conexão
-    conn = DriverManager.getConnection("jdbc:derby://localhost:1527/cadastro_contatos;SecurityMechanism=3",
-            "git", // usuario
-            "git"); // senha
-    return conn;
-  }
+    public Scanner entrada = new Scanner(System.in);
 
-  public void listarPessoas() {
-    Statement stmt = null;
-    Connection conn = null;
-
-    String sql = "SELECT NOME, DATA_NASCIMENTO, TELEFONE, EMAIL FROM CADASTRO_CONTATO";
-    try {
-      conn = obterConexao();
-      stmt = conn.createStatement();
-      ResultSet resultados = stmt.executeQuery(sql);
-
-      
-
-      while (resultados.next()) {
-
-        String nome = resultados.getString("NOME");
-        String dataNasc = resultados.getString("DATA_NASCIMENTO");
-        String email = resultados.getString("EMAIL");
-        String telefone = resultados.getString("TELEFONE");
-        System.out.println(nome+ ", " +dataNasc+", "+email + ", " + telefone);
-      }
-
-    } catch (SQLException ex) {
-      Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ClassNotFoundException ex) {
-      Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-    } finally {
-      if (stmt != null) {
-        try {
-          stmt.close();
-        } catch (SQLException ex) {
-          Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      }
-      if (conn != null) {
-        try {
-          conn.close();
-        } catch (SQLException ex) {
-          Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      }
+    private Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        // Passo 1: Registrar driver JDBC.
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
+        // Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection("jdbc:derby://localhost:1527/cadastro_contatos;SecurityMechanism=3",
+                "git", // usuario
+                "git"); // senha
+        return conn;
     }
-  }
 
-  public void incluirPessoa() {
-    PreparedStatement stmt = null;
-    Connection conn = null;
+    public void listarPessoas() {
+        Statement stmt = null;
+        Connection conn = null;
 
-    String nome;
-    String email;
-    String telefone;
-
-    // ENTRADA DE DADOS
-    Scanner entrada = new Scanner(System.in);
-    System.out.print("Digite o nome da pessoa: ");
-    nome = entrada.nextLine();
-
-    System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
-    String strDataNasc = entrada.nextLine();
-   
-    System.out.print("Digite o telefone: ");
-    telefone = entrada.nextLine();
-
-    System.out.print("Digite o e-mail: ");
-    email = entrada.nextLine();
-
-    String sql = "INSERT INTO CADASTRO_CONTATO (NOME, DATA_NASCIMENTO, TELEFONE, EMAIL) VALUES ('"+nome+"', '"+strDataNasc+"', '"+telefone+"', '"+email+"')";
-    try {
-       conn = obterConexao();
-       Statement stm;
-       stm = conn.createStatement();
-       stm.executeUpdate(sql);
-
-      System.out.println("Registro incluido com sucesso.");
-
-    } catch (SQLException ex) {
-      Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ClassNotFoundException ex) {
-      Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-    } finally {
-      if (stmt != null) {
+        String sql = "SELECT NOME, DATA_NASCIMENTO, TELEFONE, EMAIL FROM CADASTRO_CONTATO";
         try {
-          stmt.close();
+            conn = obterConexao();
+            stmt = conn.createStatement();
+            ResultSet resultados = stmt.executeQuery(sql);
+
+            while (resultados.next()) {
+                String nome = resultados.getString("NOME");
+                String dataNasc = resultados.getString("DATA_NASCIMENTO");
+                String email = resultados.getString("EMAIL");
+                String telefone = resultados.getString("TELEFONE");
+                System.out.println(nome + ", " + dataNasc + ", " + email + ", " + telefone);
+            }
         } catch (SQLException ex) {
-          Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
-      }
-      if (conn != null) {
-        try {
-          conn.close();
-        } catch (SQLException ex) {
-          Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      }
     }
-  }
- public void editarPessoa() {
-    PreparedStatement stmt = null;
-    Connection conn = null;
 
-    String nome;
-    String email;
-    String telefone;
+    public void incluirPessoa() {
+        String nome, email, telefone;
 
-    // ENTRADA DE DADOS
-    Scanner entrada = new Scanner(System.in);
-    System.out.print("Digite o nome da pessoa: ");
-    nome = entrada.nextLine();
+        // ENTRADA DE DADOS
+        System.out.print("Digite o nome da pessoa: ");
+        nome = entrada.nextLine();
+        System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
+        String strDataNasc = entrada.nextLine();
+        System.out.print("Digite o telefone: ");
+        telefone = entrada.nextLine();
+        System.out.print("Digite o e-mail: ");
+        email = entrada.nextLine();
 
-    System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
-    String strDataNasc = entrada.nextLine();
- 
-    System.out.print("Digite o telefone: ");
-    telefone = entrada.nextLine();
+        String sql = "INSERT INTO CADASTRO_CONTATO (NOME, DATA_NASCIMENTO, TELEFONE, EMAIL) VALUES ('" + nome + "', '" + strDataNasc + "', '" + telefone + "', '" + email + "')";
+        conecta(sql, "Registro incluido com sucesso!");
 
-    System.out.print("Digite o e-mail: ");
-    email = entrada.nextLine();
-
-    String sql = "UPDATE CADASTRO_CONTATO SET NOME='"+nome+"', DATA_NASCIMENTO='"+strDataNasc+"', TELEFONE='"+telefone+"', EMAIL='"+email+"' WHERE NOME = '"+nome+"'";
-    try {
-       conn = obterConexao();
-       Statement stm;
-       stm = conn.createStatement();
-       stm.executeUpdate(sql);
-
-      System.out.println("Registro alterado com sucesso.");
-
-    } catch (SQLException ex) {
-      Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ClassNotFoundException ex) {
-      Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-    } finally {
-      if (stmt != null) {
-        try {
-          stmt.close();
-        } catch (SQLException ex) {
-          Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      }
-      if (conn != null) {
-        try {
-          conn.close();
-        } catch (SQLException ex) {
-          Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      }
     }
-  }
 
-  public static void main(String[] args) {
- 
-  }
+    public void editarPessoa() {
+        String nome, email, telefone;
+
+        // ENTRADA DE DADOS
+        System.out.print("Digite o nome da pessoa: ");
+        nome = entrada.nextLine();
+        System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
+        String strDataNasc = entrada.nextLine();
+        System.out.print("Digite o telefone: ");
+        telefone = entrada.nextLine();
+        System.out.print("Digite o e-mail: ");
+        email = entrada.nextLine();
+
+        String sql = "UPDATE CADASTRO_CONTATO SET NOME='" + nome + "', DATA_NASCIMENTO='" + strDataNasc + "', TELEFONE='" + telefone + "', EMAIL='" + email + "' WHERE NOME = '" + nome + "'";
+        conecta(sql, "Registro alterado com sucesso!");
+    }
+
+    public void removerPessoa() {
+        String nome;
+        // REMOÇÃO
+        System.out.print("Digite o nome da pessoa que sera removido: ");
+        nome = entrada.nextLine();
+
+        conecta("NOME, DATA_NASCIMENTO, TELEFONE, EMAIL FROM CADASTRO_CONTATO WHERE NOME='" + nome + "'", "Confirmar remoção?\n1-Sim\n2-Não");
+
+        if (entrada.nextInt() == 1) { // Confirmação de exclusão
+            conecta("DELETE FROM CADASTRO_CONTATO WHERE NOME='" + nome + "'", "Registro removido com sucesso!");
+        } else {
+            System.out.println("Operaçao cancelada!");
+        }
+    }
+
+    // Função "conecta" criada para otimizar, e reduzir quantidade de códigos repetidos
+    public void conecta(String sql, String retorno) {
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        try {
+            conn = obterConexao();
+            Statement stm;
+            stm = conn.createStatement();
+            stm.executeUpdate(sql); // Comando de execução
+
+            System.out.println(retorno); // Print na tela a mensagem de conclusão
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
 }
--- src/main/java/br/senac/tads/pi3/ricardo/oliveira/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/ricardo/oliveira/exercicio01/Exercicio01.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.senac.tads.pi3.ricardo.oliveira.exercicio01;
 
 import java.util.Scanner;
 
-
 /**
- *
  * @author ricardo.oliveira5
  */
 public class Exercicio01 {
@@ -18,26 +11,25 @@ public class Exercicio01 {
         Scanner leitor = new Scanner(System.in);
         int opc;
         Conexao instancia = new Conexao();
-           /*usuario:git senha:git*/ 
+        /*usuario:git senha:git*/
         do {
-            System.out.println("1 - Cadastrar");//insert
-            System.out.println("2 - Editar");//update
-            System.out.println("3 - Remover");//delete
-            System.out.println("4 - Listar");//select
+            System.out.println("1 - Cadastrar"); // Insert
+            System.out.println("2 - Editar"); // Update
+            System.out.println("3 - Remover"); // Delete
+            System.out.println("4 - Listar"); // Select
             System.out.println("5 - Sair");
             opc = leitor.nextInt();
 
             if (opc == 1) {
-             instancia.incluirPessoa();
-            }if(opc == 2){
-             instancia.editarPessoa();
-            }if(opc == 3){
-            
-            }if(opc == 4){
-            instancia.listarPessoas();
+                instancia.incluirPessoa();
+            } else if (opc == 2) {
+                instancia.editarPessoa();
+            } else if (opc == 3) {
+                instancia.listarPessoas();
+                instancia.removerPessoa();
+            } else if (opc == 4) {
+                instancia.listarPessoas();
             }
-        
-        }while (opc != 5);
-
+        } while (opc != 5);
     }
 }
